2e13a1d66213f817aa733effe4960ad10003fb7c
Pepe García <pgarciag93@gmail.com>
2019-12-30 14:13:08 +0100

    spark and homestead

--- aliases.zsh
+++ aliases.zsh
@@ -2,7 +2,7 @@
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadcli="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -ahlF --color --group-directories-first"
+alias ll="ls -ahlF"
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
 alias zbundle="antibody bundle < $DOTFILES/zsh_plugins.txt > $DOTFILES/zsh_plugins.sh"
@@ -35,3 +35,7 @@ alias gc="git checkout"
 alias gd="git diff"
 alias gl="git log --oneline --decorate --color"
 alias gnuke="git clean -df && git reset --hard"
+
+function homestead() {
+    ( cd ~/Homestead && vagrant $* )
+}
--- path.zsh
+++ path.zsh
@@ -4,6 +4,9 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 # Load Node global installed binaries
 export PATH="$HOME/.node/bin:$PATH"
 
+#Load spark
+export PATH="$HOME/.spark:$PATH"
+
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 # Make sure coreutils are loaded before system commands
